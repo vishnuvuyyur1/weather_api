4f25ec725094094ec0306b4926b2c19505de4572
guru <guru.cse1@gmail.com>
2025-03-19 11:02:08 +0100

    added post endpoint to save weather

--- src/main/java/com/klm/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/klm/weather/controller/WeatherApiRestController.java
@@ -1,9 +1,27 @@
 package com.klm.weather.controller;
 
-import org.springframework.web.bind.annotation.*;
+import com.klm.weather.model.Weather;
+import com.klm.weather.repository.WeatherRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherApiRestController {
+    private static final String PATH = "/weather";
 
+    private final WeatherRepository weatherRepository;
+
+    public WeatherApiRestController(WeatherRepository weatherRepository) {
+        this.weatherRepository = weatherRepository;
+    }
+
+    @PostMapping(PATH)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Weather addWeather(@RequestBody Weather weather) {
+            return weatherRepository.save(weather);
+    }
 
 }
--- src/main/java/com/klm/weather/model/Weather.java
+++ src/main/java/com/klm/weather/model/Weather.java
@@ -1,6 +1,8 @@
 package com.klm.weather.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.Date;
@@ -10,6 +12,7 @@ import java.util.List;
 public class Weather {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private Date date;
